ff73ff648ba727e17410448a14ad91ddd7a1bfc8
fumtas1k <fumta01a7@gmail.com>
2022-03-03 00:07:55 +0900

    add:basic認証追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
+  before_action :basic_auth unless Rails.env.test?
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |name, password|
+      name == ENV["BASIC_AUTH_NAME"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
